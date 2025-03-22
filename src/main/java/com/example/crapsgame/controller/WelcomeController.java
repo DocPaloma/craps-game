c69db4f2b35bea4f89a2bf9c77c037b87a357a74
Alejandro Medina <weaqla.xd@gmail.com>
2025-03-22 10:49:18 -0500

    si

--- src/main/java/com/example/crapsgame/controller/WelcomeController.java
+++ src/main/java/com/example/crapsgame/controller/WelcomeController.java
@@ -1,9 +1,11 @@
 package com.example.crapsgame.controller;
 
+import com.example.crapsgame.models.AlertBox;
 import com.example.crapsgame.models.Player;
 import com.example.crapsgame.view.GameView;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.scene.control.Alert;
 import javafx.scene.control.TextField;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
@@ -29,6 +31,8 @@ public class WelcomeController {
         Player player = new Player();
         player.setNickname(nickname);
 
+        AlertBox alertBox = new AlertBox();
+        alertBox.showAlertBox("Are you ready?", "Are you ready?", "Are you ready?");
         GameView gameView = getInstance();
         gameView.getGameController().setPlayer(player);
         gameView.show();
